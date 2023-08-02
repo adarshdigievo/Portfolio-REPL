1c9c0a7bfd7a3834c0fa2f03655efba0d67353b1
Adarsh D <adarshdevamritham@gmail.com>
2023-08-02 12:00:40 +0530

    Initial commit - Pyscript version. Removed old dependencies.

---
+++ pyscript_code.py
@@ -0,0 +1,190 @@
+import enum
+import sys
+import asyncio
+import webbrowser
+import code
+from dataclasses import dataclass
+from datetime import datetime
+
+from pyodide.http import pyfetch
+
+version_string = None  # Version string variable is exported as a global variable. Value is assigned when the py-script main() is executed.
+
+ProfileData = None  # Class with descriptor attributes, for loading profile data. Initialised from main()
+
+interpreter = code.InteractiveInterpreter()  # runs user commands
+
+
+def execute_command(command: str) -> str:
+    import io
+    from contextlib import redirect_stdout, redirect_stderr
+
+    # stdout and stderr are redirected to a string to be given as return to the js call
+    with io.StringIO() as buf, redirect_stderr(buf):
+        with io.StringIO() as buf1, redirect_stdout(buf1):
+            for field in profile_fields_list:
+                if field in command:
+                    command = command.replace(
+                        field, repr(getattr(ProfileData, field.lower()))
+                    )
+            compiled_code = code.compile_command(
+                command
+            )  # If compile code is not used, variable repr are not printed
+            interpreter.runcode(compiled_code)
+            output = buf1.getvalue()
+
+        output = output or buf.getvalue()
+        output = "\n\r".join(output.split("\n"))
+
+    return output
+
+
+# Used to store the profile data, along with the time it was fetched
+# This is used to re-fetch the data after a certain amount of time has passed (currently 5 minutes)
+@dataclass
+class ProfileDataRecord:
+    data_dict: dict
+    refreshed_timestamp: float
+
+
+# Enum defining profile categories
+class ProfileFields(enum.Enum):
+    NAME = enum.auto()
+    ABOUT = enum.auto()
+    SKILLS = enum.auto()
+    EXPERIENCE = enum.auto()
+    CONFERENCE_TALKS = enum.auto()
+    OPEN_SOURCE_CONTRIBUTIONS = enum.auto()
+    EDUCATION = enum.auto()
+    CERTIFICATIONS = enum.auto()
+
+
+profile_fields_list = [
+    member.name for member in ProfileFields
+]  # Used to show the pre-loaded variables
+
+
+# Descriptor class for fetching profile data. Used to make variable loading dynamic
+class ProfileFetchDescriptor:
+    profile_data: ProfileDataRecord | None = None
+
+    @classmethod
+    async def load_profile_data(cls) -> None:
+        # Get profile data by calling gitconnected API and store it as a class attribute
+        resp = await pyfetch("https://gitconnected.com/v1/portfolio/adarshdigievo")
+        cls.profile_data = ProfileDataRecord(
+            data_dict=await resp.json(), refreshed_timestamp=datetime.now().timestamp()
+        )
+
+    def __init__(self, field: ProfileFields) -> None:
+        self.field = field
+
+    def __get__(self, _, __) -> str:
+        current_timestamp = datetime.now().timestamp()
+        if (
+            current_timestamp - ProfileFetchDescriptor.profile_data.refreshed_timestamp
+            > 300
+        ):
+            asyncio.run(self.load_profile_data())
+
+        match self.field:
+            case ProfileFields.NAME:
+                return ProfileFetchDescriptor.profile_data.data_dict["basics"]["name"]
+
+            case ProfileFields.ABOUT:
+                about = ""
+                about += (
+                    f"{ProfileFetchDescriptor.profile_data.data_dict['basics']['name']} \n{ProfileFetchDescriptor.profile_data.data_dict['basics']['label']} \n"
+                    ""
+                )
+                about += f"{ProfileFetchDescriptor.profile_data.data_dict['basics']['summary']}\n"
+                about += "\nSocial Profiles: \n"
+                for profile_data in ProfileFetchDescriptor.profile_data.data_dict[
+                    "basics"
+                ]["profiles"]:
+                    about += f"{profile_data['network']}: {profile_data['username']}\n{profile_data['url']} \n\n"
+                return str(about)
+
+            case ProfileFields.SKILLS:
+                skill_list = ProfileFetchDescriptor.profile_data.data_dict["skills"]
+                return "\n".join(
+                    f"{skill['name']} {'☆ ' * skill['rating']}" for skill in skill_list
+                )
+
+            case ProfileFields.EXPERIENCE:
+                exp = ""
+                for exp_data in ProfileFetchDescriptor.profile_data.data_dict["work"]:
+                    exp += f"\n{'-' * 30}\n{exp_data['position']} at {exp_data['name']}  | {exp_data['startDate']} - {exp_data['endDate']} | {exp_data['website']} \n\n"
+                    exp += f"{exp_data['summary']} \n"
+                    if exp_data["highlights"]:
+                        exp += "\n".join(
+                            f"- {highlight}" for highlight in exp_data["highlights"]
+                        )
+                    exp += "\n\n"
+                return exp
+
+            case ProfileFields.CONFERENCE_TALKS:
+                talks = ""
+                for talk in ProfileFetchDescriptor.profile_data.data_dict[
+                    "publications"
+                ]:
+                    talks += f"\n{talk['name']}\n"
+                    talks += f"{talk['url']} \n{talk['summary']} \n"
+                return talks
+
+            case ProfileFields.OPEN_SOURCE_CONTRIBUTIONS:
+                oss_contributions = ""
+                for contrib in ProfileFetchDescriptor.profile_data.data_dict[
+                    "projects"
+                ]:
+                    oss_contributions += f"\n☆ {contrib['name']}\n"
+                    oss_contributions += (
+                        f"{contrib['githubUrl']} \n{contrib['summary']} \n"
+                    )
+                return oss_contributions
+
+            case ProfileFields.EDUCATION:
+                education = ""
+                for edu in ProfileFetchDescriptor.profile_data.data_dict["education"]:
+                    education += f"\n{edu['institution']} | {edu['area']} | {edu['studyType']} | {edu['startDate']} - {edu['endDate']} \n"
+                    education += f"{edu['description']} \n"
+                return education
+
+            case ProfileFields.CERTIFICATIONS:
+                return "".join(
+                    f"\n{cert['name']} | {cert['issuer']} \n"
+                    for cert in ProfileFetchDescriptor.profile_data.data_dict[
+                        "certificates"
+                    ]
+                )
+            case _:
+                return "WIP"
+
+
+async def main():
+    await ProfileFetchDescriptor.load_profile_data()
+
+    attrs = {
+        field.name.lower(): ProfileFetchDescriptor(field) for field in ProfileFields
+    }
+
+    global ProfileData
+
+    # Dynamically generate a class named ProfileData. Each member attribute of the class is a descriptor
+    ProfileData = type("ProfileData", (), attrs)
+
+    global site_description_string  # making it global, so that it can be accessed from js, via pyodide interpreter globals
+    site_description_string = "Adarsh Divakaran | Portfolio REPL\n\n\r"
+    site_description_string += (
+        f'Pre-loaded variables: {", ".join(profile_fields_list)}.\n\r'
+    )
+    site_description_string += f"Try entering `print(ABOUT)` in the REPL\n\n\r"
+
+    global version_string
+    version_string = f"Python {sys.version}\n\r"
+    version_string += (
+        'Type "help", "copyright", "credits" or "license" for more information.\n\r'
+    )
+
+
+asyncio.ensure_future(main())
---
+++ xterm_logic.js
@@ -0,0 +1,106 @@
+function ensurePyscriptLoaded() {
+
+    return new Promise(waitForPyscript);
+
+    function waitForPyscript(resolve, reject) {
+        if (pyscript && pyscript.interpreter && pyscript.interpreter.globals.get("version_string"))
+            resolve();
+        else
+            setTimeout(waitForPyscript.bind(this, resolve, reject), 30);
+    }
+}
+		var term = new Terminal({
+			cursorBlink: true,
+			cursorStyle: "bar"
+		});
+
+        var curr_line = ''; // holds command being entered
+		var entries = []; // stores command history
+		var currPos = 0;  // current position in entries array
+		var pos = 0; // tracks cursor position in curr_line
+
+		term.open(document.getElementById('terminal'));
+
+		term.prompt = () => {
+			term.write('\n\r' + curr_line + '\r\n>>>  ');
+		};
+
+        ensurePyscriptLoaded().then(function(){
+            console.log("Pyscript Loaded");
+            term.write(pyscript.interpreter.globals.get("site_description_string"));
+            term.write(pyscript.interpreter.globals.get("version_string"));
+            term.prompt();
+            term.focus();
+        });
+
+		term.on("key", function(key, ev) {
+			const printable = !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey &&
+				!(ev.keyCode === 37 && term.buffer.cursorX < 6);
+
+			if (ev.keyCode === 13) { // Enter key
+				if (curr_line.replace(/^\s+|\s+$/g, '').length != 0) { // Check if string is all whitespace
+					entries.push(curr_line);
+					currPos = entries.length;
+
+                    // when enter is pressed, call the execute_command python function defined in pyscript with the current command
+					term.write('\n\r' + pyscript.interpreter.globals.get('execute_command')(curr_line));
+					term.write('\n\33[2K\r>>>  '); // \33[2K cleans the current line
+
+				} else { // entry is whitespace only
+					term.write('\n\33[2K\r>>>  '); //  \33[2K cleans the current line
+				}
+				curr_line = '';
+			} else if (ev.keyCode === 8) { // Backspace
+				if (term.buffer.cursorX > 5) { // checks if the cursor is not at start position
+
+                    // Remove the character before the cursor and update `curr_line`
+					curr_line = curr_line.slice(0, term.buffer.cursorX - 6) + curr_line.slice(term.buffer.cursorX - 5);
+
+					// Calculate the new cursor position (`pos`) after removing the character
+					pos = curr_line.length - term.buffer.cursorX + 6;
+					term.write('\33[2K\r>>>  ' + curr_line);
+					term.write('\033['.concat(pos.toString()).concat('D')); // This moves the cursor `pos` columns to the left
+
+					// Check if the cursor is at the start or end of the line
+                    // If yes, move the cursor one position to the right
+					if (term.buffer.cursorX == 5 || term.buffer.cursorX == curr_line.length + 6) {
+						term.write('\033[1C')
+					}
+				}
+			} else if (ev.keyCode === 38) { // Up arrow
+				if (entries.length > 0) {
+					if (currPos > 0) {
+						currPos -= 1;
+					}
+					curr_line = entries[currPos];
+					term.write('\33[2K\r>>>  ' + curr_line);
+				}
+			} else if (ev.keyCode === 40) { // Down arrow
+				currPos += 1;
+				if (currPos === entries.length || entries.length === 0) {
+					currPos -= 1;
+					curr_line = '';
+					term.write('\33[2K\r>>>  ');
+				} else {
+					curr_line = entries[currPos];
+					term.write('\33[2K\r>>>  ' + curr_line);
+
+				}
+
+			}
+			// For other printable keys (non-control, non-arrow keys), If the cursor is not at the end of the line the pressed key is inserted into the curr_line at the appropriate cursor position.
+			else if (printable && !(ev.keyCode === 39 && term.buffer.cursorX > curr_line.length + 4)) {
+				if (ev.keyCode != 37 && ev.keyCode != 39) {
+					var input = ev.key;
+					if (ev.keyCode == 9) { // Tab
+						input = "    ";
+					}
+					pos = curr_line.length - term.buffer.cursorX + 4;
+					curr_line = [curr_line.slice(0, term.buffer.cursorX - 5), input, curr_line.slice(term.buffer.cursorX - 5)].join('');
+					term.write('\33[2K\r>>>  ' + curr_line);
+					term.write('\033['.concat(pos.toString()).concat('D')); // moving cursor to present position
+				} else {
+					term.write(key);
+				}
+			}
+		});
